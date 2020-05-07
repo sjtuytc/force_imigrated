Fix bugs in np_physics_env. 1) func apply order calculate_surface_normals 2) quaternion to matrix function. Scalar first needs to be transferred to scalar last.

baselines/LukeForce/debug/test_physics_env.py
@@ -5,10 +5,11 @@ import logging
 import os
 import time
 from environments.np_physics_env import NpPhysicsEnv
-from utils.environment_util import NpForceValOnly, NpEnvState
+from environments.physics_env import PhysicsEnv
+from utils.environment_util import NpForceValOnly, NpEnvState, ForceValOnly, EnvState
 
 
-def test_original():
+def test_speed():
     args = parse_args()
 
     random.seed(args.seed)
@@ -53,6 +54,52 @@ def test_original():
     print("Time per call:", total_t / 2000)
 
 
+def compare_to_original():
+    args = parse_args()
+
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    root_dir = args.data
+    object_list = args.object_list
+
+    object_paths = {obj: os.path.join(root_dir, 'objects_16k', obj, 'google_16k', 'textured.urdf') for
+                    obj in object_list}
+
+    test_obj = '005_tomato_soup_can'
+    np_phy_env = NpPhysicsEnv(render=args.render, object_name=test_obj, object_path=object_paths[test_obj],
+                                gravity=args.gravity, debug=args.debug, number_of_cp=args.number_of_cp, fps=args.fps,
+                                force_multiplier=args.force_multiplier,
+                                force_h=args.force_h, state_h=args.state_h, qualitative_size=args.qualitative_size,
+                                workers=0)
+    np_phy_env.reset()
+
+    # generate test data.
+    forces = [ForceValOnly(force=(-0.0047, -0.0841, -0.0801)).tolist() for i in range(5)]
+    initial_state = EnvState(object_name='005_tomato_soup_can', position=(-0.2421,  0.0213,  0.9691),
+                               rotation=(0.3126, -0.5557,  0.4300, -0.6392),
+                               velocity=(0.0, 0.0, 0.0), omega=(0., 0., 0.)).to_dict()
+
+    list_of_contact_points = [[-0.0934, -0.0214,  0.0495], [-0.0651, -0.0909, -0.0848], [-0.0042,  0.0523, -0.0092],
+                              [0.0637, -0.0939, -0.0402], [0.0107,  0.0885,  0.0101]]
+    current_state, list_of_force_success, list_of_force_location = \
+        np_phy_env.init_location_and_apply_force(forces=forces, initial_state=initial_state, object_num=None,
+                                                     list_of_contact_points=list_of_contact_points)
+
+
+    phy_env = PhysicsEnv(render=args.render, object_name=test_obj, object_path=object_paths[test_obj], gravity=args.gravity,
+                         debug=args.debug, number_of_cp=args.number_of_cp, fps=args.fps, force_multiplier=args.force_multiplier,
+                         force_h=args.force_h, state_h=args.state_h, qualitative_size=args.qualitative_size,
+                         workers=0, gpu_ids=0)
+    phy_env.reset()
+    new_state, new_force_success, new_force_location = \
+        phy_env.init_location_and_apply_force(forces=forces, initial_state=initial_state, object_num=None,
+                                              list_of_contact_points=list_of_contact_points)
+
+    print("Np env:", current_state, list_of_force_success, list_of_force_location)
+    print("Ori env:", new_state, new_force_success, new_force_location)
+
+
 if __name__ == '__main__':
     print("Test in one function.")
-    test_original()
+    compare_to_original()

baselines/LukeForce/environments/np_physics_env.py
@@ -62,7 +62,7 @@ class NpPhysicsEnv(BaseBulletEnv):
             face = vertex_to_face_dict[vert_ind][0].tolist()
             vertices_on_this_face = vertices[face]
             surface_normal = np.cross((vertices_on_this_face[1] - vertices_on_this_face[0]) * 10,
-                                        (vertices_on_this_face[2] - vertices_on_this_face[0]) * 10)
+                                      (vertices_on_this_face[2] - vertices_on_this_face[0]) * 10)
             # To avoid numerical errors because these are all very close
             surface_normal = surface_normal / np.linalg.norm(surface_normal)
             all_normals.append(surface_normal)
@@ -81,7 +81,8 @@ class NpPhysicsEnv(BaseBulletEnv):
         elif side_of_force > 0:
             # force is applied tangentically
             a_cross_b = np.cross(force_value, surface_normal)
-            projected_force = np.cross(surface_normal, a_cross_b / np.linalg.norm(surface_normal) / np.linalg.norm(surface_normal))
+            projected_force = np.cross(surface_normal, a_cross_b / np.linalg.norm(surface_normal)) / \
+                              np.linalg.norm(surface_normal)
             return 0.5, projected_force
         else:
             return 0, None
@@ -169,8 +170,11 @@ class NpPhysicsEnv(BaseBulletEnv):
         return rotated_vertices + position.reshape(1, 3)
 
     def get_rotation_mat(self, state):
-        quat = R.from_quat(state.rotation.reshape(1, 4))
-        return quat.as_matrix().squeeze(0)
+        # transfer to scalar-last format.
+        scalar_first = state.rotation.tolist()
+        scalar_last = scalar_first[1:] + [scalar_first[0]]
+        quat = R.from_quat(scalar_last)
+        return quat.as_matrix()
 
     def get_current_state(self, object_num=None):
         if object_num is None:

baselines/LukeForce/utils/net_util.py
@@ -297,13 +297,13 @@ class BatchCPGradientLayer(Function):
                         1 + len(contact_point_tensor) + len(forces_tensor) + len(state) - 1)]
             tweak_cp_state_tensors, tweak_force_state_tensors, tweak_state_tensors = \
                 [all_state_tensors[a: b] for a, b in indexes]
-            cp_fd = [(tweaked - env_state_tensor) / tweak_value_cp for tweaked in tweak_cp_state_tensors]
+            cp_fd = [(env_state_tensor - tweaked) / tweak_value_cp for tweaked in tweak_cp_state_tensors]
             if no_grad_on:
                 finite_diff_success_flags = [all_succ_flags[0]]
             else:
                 finite_diff_success_flags = all_succ_flags[1 + len(contact_point_tensor):] + [all_succ_flags[0]]
-            force_fd = [(tweaked - env_state_tensor) / tweak_value_force for tweaked in tweak_force_state_tensors]
-            state_fd = [(tweaked - env_state_tensor) / tweak_value_state for tweaked in tweak_state_tensors] + \
+            force_fd = [(env_state_tensor - tweaked) / tweak_value_force for tweaked in tweak_force_state_tensors]
+            state_fd = [(env_state_tensor - tweaked) / tweak_value_state for tweaked in tweak_state_tensors] + \
                        [torch.zeros([len(state)])]
             if not no_grad_on:
                 ctx.contact_pt_x_plus_h_diff = torch.stack(cp_fd, dim=-2)
@@ -316,9 +316,9 @@ class BatchCPGradientLayer(Function):
         finite_diff_success_flags = torch.Tensor(finite_diff_success_flags).to(device=device)
         env_state_tensor = env_state_tensor.to(device=device)
         if not no_grad_on:  # if require grads.
+            ctx.contact_pt_x_plus_h_diff = ctx.contact_pt_x_plus_h_diff.to(device=device)
             ctx.f_x_plus_h_diff = ctx.f_x_plus_h_diff.to(device=device)
             ctx.initial_state_plus_h_diff = ctx.initial_state_plus_h_diff.to(device=device)
-            ctx.contact_pt_x_plus_h_diff = ctx.contact_pt_x_plus_h_diff.to(device=device)
         return env_state_tensor, finite_diff_success_flags, force_that_applied
 
     @staticmethod